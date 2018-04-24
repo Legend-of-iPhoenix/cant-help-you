3006f3c5c25f1cb7047b9d53228e0c21307e3869
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-04-24 19:15:00 -0400

    Major update.
    - Update creator.
    - Add viewer.

--- creator/js/main.js
+++ creator/js/main.js
@@ -1,18 +1,3 @@
-// List of reasons. In JSON format (key: value pairs) where the key is a helpful identifier and the value is valid HTML shown to the user.
-var reasons = [
-	"I am working on other projects right now.",
-	"Your project just does not appeal to me.",
-	"I don't know how to solve your problem, so I cannot help you.",
-	"You have put minimal effort into solving the problem yourself. You should use your favorite search engine to find your information. <a href='https://www.google.com/'>Google</a> works nicely :)",
-	"You have ignored my advice on the subject.",
-	"You have been unhelpful or even hostile when I tried to help you with your project. How can I help you if you won't let me?",
-	"The information is plainly listed in the documentation, which you have obviously not checked.",
-	"I'm really tired right now. I'm going to place my personal health above the immediate success of your project.",
-	"You got me at the wrong time. I have other things to do right now.",
-	"I'm not going to help you, because it will be a good learning experience for you. You will learn better if you try to do it yourself than if I were to just give you the answer.",
-	"You ask for help on every single point. I'm not going to do your project for you."
-]
-
 var selected = reasons.map(x=>0);
 
 // shows the creator.
---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+	var reasonData = location.search.split('=')[1];
+	if (reasonData === undefined || reasonData == "0") {
+		location.href = "creator/"
+	} else {
+		var reasonList = [];
+		reasonData = parseInt(reasonData, 36).toString(2).split(''); // convert to binary
+		if (1 == reasonData.reduce((accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue))) { // if only one reason was selected
+			var reasonString = reasons[reasonData.indexOf("1")];
+
+			document.getElementById("reasonStart").innerHTML = " because "+(["I","I'm"].indexOf(reasonString.match(/\w*/)[0]) === -1 ? reasonString.charAt(0).toLowerCase() : "I") + reasonString.substring(1);
+		} else {
+			reasonData.forEach(function(value, index) {
+				// if the binary bit is set to 1, add the reason to the list.
+				if (value === "1") {
+					var li = document.createElement("li");
+					li.innerHTML = reasons[index];
+					document.getElementById("reasons").appendChild(li)
+					document.getElementById("reasons").innerHTML += "<br>";
+				}
+			});
+		}
+	}
+}
